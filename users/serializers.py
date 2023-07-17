abc99f375ce9a801b4dfab171fadf86594c21d80
Marina <vorona.2003.bu@yandex.ru>
2023-07-17 22:01:10 +0300

    add Meta to user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -10,15 +10,16 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     """Сериализатор модели пользователя."""
 
-    ...
 #     is_subscribed = serializers.SerializerMethodField()
 
-#     class Meta:
-#         model = User
-#         fields = (
-#             'id', 'username', 'email', 'first_name', 'last_name',
-#             'is_subscribed'
-#         )
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'email',
+            'first_name',
+        )
 
 #     def get_is_subscribed(self, obj):
 #         user = self.context['request'].user
