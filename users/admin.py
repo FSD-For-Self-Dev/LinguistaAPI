5ccc4392865b1d25e79f1c181c3aa6a94ead9562
Marina <vorona.2003.bu@yandex.ru>
2023-07-17 21:49:39 +0300

    fix admin register user; add docstring

--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,9 @@ from django.contrib import admin
 from .models import User
 
 
-@admin.site.register(User)
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    """Админ-панель модели пользователя"""
+
     list_display = ('username', 'email')
     list_filter = ('username', 'email')
