da0c190d6823806e7e4a25d3c0989f56ee055b76
Marina <vorona.2003.bu@yandex.ru>
2023-08-15 16:32:02 +0300

    add SPECTACULAR_SETTINGS

--- linguista/linguista/settings.py
+++ linguista/linguista/settings.py
@@ -155,6 +155,19 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Linguista API',
+    'DESCRIPTION': 'API endpoints for Linguista app',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    "SWAGGER_UI_SETTINGS": {
+        "filter": True,  # включить поиск по тегам
+    },
+    "COMPONENT_SPLIT_REQUEST": True,
+}
+
+
 LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
