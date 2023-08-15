833f4f595f9aa8bacab19e0f1cacec4fe911d892
Marina <vorona.2003.bu@yandex.ru>
2023-08-15 23:23:25 +0300

    update users app config

--- linguista/users/apps.py
+++ linguista/users/apps.py
@@ -1,9 +1,10 @@
 ''' Users app config '''
 
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
-    verbose_name = 'Пользователи'
+    verbose_name = _('Users')
