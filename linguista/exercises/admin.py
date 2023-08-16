5a37ade61805a8e571d4371a75dc12d78d4fa4ed
Marina <vorona.2003.bu@yandex.ru>
2023-08-16 10:48:52 +0300

    add FavoriteExercises model to admin

--- linguista/exercises/admin.py
+++ linguista/exercises/admin.py
@@ -2,6 +2,7 @@
 
 from django.contrib import admin
 
-from .models import Exercise
+from .models import Exercise, FavoriteExercise
 
 admin.site.register(Exercise)
+admin.site.register(FavoriteExercise)
