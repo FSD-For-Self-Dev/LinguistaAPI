ce0ba3672186491778dca1b9326fdb544784c8ae
Marina <vorona.2003.bu@yandex.ru>
2023-08-16 10:54:28 +0300

    update vocabulary admin

--- linguista/vocabulary/admin.py
+++ linguista/vocabulary/admin.py
@@ -2,11 +2,23 @@
 
 from django.contrib import admin
 
-from .models import Collection, Tag, Translation, Type, UsageExample, Word
+from .models import (Collection, Definition, FavoriteCollection, FavoriteWord,
+                     Note, Synonyms, Tag, Translation, Type, UsageExample,
+                     Word, WordDefinitions, WordsInCollections,
+                     WordTranslations, WordUsageExamples)
 
 admin.site.register(Word)
 admin.site.register(Translation)
+admin.site.register(WordTranslations)
 admin.site.register(UsageExample)
+admin.site.register(WordUsageExamples)
+admin.site.register(Definition)
+admin.site.register(WordDefinitions)
 admin.site.register(Tag)
 admin.site.register(Type)
 admin.site.register(Collection)
+admin.site.register(WordsInCollections)
+admin.site.register(Synonyms)
+admin.site.register(Note)
+admin.site.register(FavoriteWord)
+admin.site.register(FavoriteCollection)
