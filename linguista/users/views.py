6258cd12617879a5a1ada8b47764c632a82f6bba
Marina <vorona.2003.bu@yandex.ru>
2023-09-07 13:37:56 +0300

    update users view

--- linguista/users/views.py
+++ linguista/users/views.py
@@ -1,17 +1,24 @@
 ''' Users views '''
 
 # from django_filters.rest_framework import DjangoFilterBackend
-# from drf_spectacular.utils import extend_schema
 from django.contrib.auth import get_user_model
 
-from djoser.views import UserViewSet as DjoserViewSet
+from drf_spectacular.utils import extend_schema
+from rest_framework import mixins, viewsets
+
+from .serializers import UserSerializer
 
 # from .filters import UserFilter
 
 User = get_user_model()
 
-# @extend_schema(tags=['Users'])
-class UserViewSet(DjoserViewSet):
-    '''Вьюсет модели пользователя.'''
-    
-    ...
+
+@extend_schema(tags=['users'])
+class UserViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    '''Список пользователей'''
+
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        """Исключение админов из выборки."""
+        return User.objects.filter(is_staff=False)
