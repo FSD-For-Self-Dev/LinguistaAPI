27d98e7d48e32893a6e65176b83215359d1ff603
onehandedpirate <plagueismkii@gmail.com>
2023-09-12 14:52:13 +1000

    move definition logic to WordViewSet actions, delete separate DefinitionViewSet therefore delete its separate router

--- linguista/vocabulary/urls.py
+++ linguista/vocabulary/urls.py
@@ -4,20 +4,15 @@ from django.urls import include, path
 
 from rest_framework import routers
 
-from .views import WordViewSet, DefinitionViewSet
+from .views import WordViewSet
 
-
-VOCABULARY_PREFIX = 'vocabulary'
 
 router = routers.DefaultRouter()
-definition_router = routers.DefaultRouter()
 
-router.register(VOCABULARY_PREFIX, WordViewSet, basename=VOCABULARY_PREFIX)
-definition_router.register('definitions', DefinitionViewSet, basename='definitions')
+router.register('vocabulary', WordViewSet, basename='vocabulary')
 # router.register('tags', TagViewSet, basename='tags')
 # router.register('users', CustomUserViewSet, basename='users')
 
 urlpatterns = [
-    path(f'{VOCABULARY_PREFIX}/<slug:slug>/', include(definition_router.urls)),
-    #path('', include(router.urls)),
+    path('', include(router.urls)),
 ]
--- linguista/vocabulary/views.py
+++ linguista/vocabulary/views.py
@@ -12,17 +12,17 @@ from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.exceptions import NotFound, PermissionDenied
 # from rest_framework.exceptions import ValidationError
-from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from core.pagination import LimitPagination
-from vocabulary.models import Word
 
-from .filters import WordFilter
+# from vocabulary.models import Word
+# from .filters import WordFilte
 from .models import Definition, WordDefinitions
-from .serializers import (TranslationSerializer, VocabularySerializer,
+from .serializers import (TranslationSerializer, WordSerializer,
                           DefinitionSerializer)
+from .permissions import CanAddDefinitionPermission
 
 User = get_user_model()
 
@@ -32,7 +32,8 @@ class WordViewSet(viewsets.ModelViewSet):
     '''Word model viewset'''
 
     # queryset = Word.objects.all()
-    serializer_class = VocabularySerializer
+    lookup_field = 'slug'
+    serializer_class = WordSerializer
     http_method_names = ['get', 'post', 'head', 'patch', 'delete']
     permission_classes = [
         IsAuthenticated,
@@ -42,6 +43,7 @@ class WordViewSet(viewsets.ModelViewSet):
     filter_backends = [
         filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend
     ]
+
     # filterset_class = WordFilter
     # search_fields = (
     #     'text', 'note', 'tags__name', 'translations__translation',
@@ -66,115 +68,83 @@ class WordViewSet(viewsets.ModelViewSet):
         '''Get random word from vocabulary'''
         queryset = self.filter_queryset(self.get_queryset())
         word = random.choice(queryset) if queryset else None
-        serializer = VocabularySerializer(
+        serializer = WordSerializer(
             word, context={'request': request}
         )
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    @action(methods=['get', 'post'], detail=True)
+    @action(
+        methods=['get', 'post'],
+        detail=True,
+        serializer_class=TranslationSerializer
+    )
     def translations(self, request, *args, **kwargs):
         '''Get all word's translations or add new translation to word'''
-        word = get_object_or_404(Word, pk=kwargs.get('pk'))
+        word = self.get_object()
         translations = word.translations.all()
         serializer = TranslationSerializer(
             translations, many=True, context={'request': request}
         )
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    @action(
+        methods=['get', 'post'],
+        detail=True,
+        serializer_class=DefinitionSerializer,
+        permission_classes=[IsAuthenticated, CanAddDefinitionPermission]
+    )
+    def definitions(self, request, *args, **kwargs):
+        """Get all definitions to a word or add new definition"""
+        word = self.get_object()
+        defs = word.definitions.all()
 
-class DefinitionViewSet(viewsets.ViewSet):
-    """Viewset for word definitions"""
-
-    permission_classes = [IsAuthenticated]
-    http_method_names = ['get', 'post', 'head', 'patch', 'delete']
-    serializer_class = DefinitionSerializer
-
-    def get_word_with_author(self):
-        """Get word and its author or 404/403 with custom message"""
-
-        word = Word.objects.filter(
-            slug=self.kwargs.get('slug')
-        ).select_related('author').first()
-        if not word:
-            raise NotFound(
-                detail="The word with the provided slug not found",
-            )
-        elif word.author != self.request.user:
-            raise PermissionDenied(
-                detail='You are not the author of this word'
-            )
-        return word, word.author
-
-    def get_obj(self):
-        definition = get_object_or_404(
-            Definition,
-            pk=self.kwargs.get('pk'),
-            worddefinitions__word__slug=self.kwargs.get('slug')
-        )
-        return definition
-
-    def list(self, request, slug=None):
-        """Get all definitions for a given word provided with a slug"""
-        word, _ = self.get_word_with_author()
-        queryset = word.definitions.all()
-        serializer = self.serializer_class(queryset, many=True)
-        return Response(serializer.data)
-
-    @extend_schema(examples=[OpenApiExample(
-        'Default',
-        value=[{'text': 'string', 'translation': 'string'}]
-    )])
-    def create(self, request, slug=None, many=True):
-        """Create new definitions for a given word"""
-
-        word, author = self.get_word_with_author()
-        serializer = self.serializer_class(data=request.data, many=True)
-        if serializer.is_valid():
-            definitions = []
-            word_definitions = []
-
-            for definition in serializer.validated_data:
-                _definition = Definition(**definition, author=author)
-                definitions.append(_definition)
-                word_definitions.append(
-                    WordDefinitions(word=word, definition=_definition)
+        match request.method:
+            case "GET":
+                return Response(
+                    self.get_serializer(defs, many=True).data,
+                    status=status.HTTP_200_OK
                 )
-            Definition.objects.bulk_create(definitions)
-            WordDefinitions.objects.bulk_create(word_definitions)
+            case "POST":
+                serializer = self.get_serializer(data=request.data)
+                if serializer.is_valid():
+                    new_def = serializer.save(
+                        author=request.user,
+                        **serializer.validated_data
+                    )
+                    WordDefinitions.objects.create(definition=new_def, word=word)
+                    return Response(self.serializer_class(new_def).data,
+                                    status=status.HTTP_201_CREATED)
 
-            return Response(
-                self.serializer_class(definitions, many=True).data,
-                status=status.HTTP_201_CREATED
-            )
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    @extend_schema(parameters=[
-        OpenApiParameter("id", int, OpenApiParameter.PATH)
-    ])
-    def partial_update(self, request, slug=None, pk=None):
-        """Partial update of a definition with PATCH request"""
+    @action(
+        detail=True,
+        methods=['get', 'patch', 'delete'],
+        url_path=r'definitions/(?P<definition_id>\d+)',
+        url_name="word's definition detail",
+        serializer_class=DefinitionSerializer,
+    )
+    def definitions_detail(self, request, *args, **kwargs):
+        """Retrieve, update or delete a word definition"""
+        word = self.get_object()
+        try:
+            definition = word.definitions.get(pk=kwargs.get('definition_id'))
+        except Definition.DoesNotExist:
+            raise NotFound(detail="The definition not found")
 
-        self.get_word_with_author()
-        obj = self.get_obj()
-        serializer = self.serializer_class(
-            instance=obj,
-            data=request.data,
-            partial=True
-        )
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    @extend_schema(parameters=[
-        OpenApiParameter("id", int, OpenApiParameter.PATH)
-    ])
-    def destroy(self, request, slug=None, pk=None):
-        """Delete a definition"""
-
-        self.get_word_with_author()
-        obj = self.get_obj()
-        obj.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        match request.method:
+            case 'GET':
+                return Response(self.get_serializer(definition).data)
+            case 'PATCH':
+                serializer = self.get_serializer(
+                    instance=definition,
+                    data=request.data,
+                    partial=True
+                )
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            case 'DELETE':
+                definition.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
