42226dbb2a8733008453c3599583aaa3a95d7433
onehandedpirate <plagueismkii@gmail.com>
2023-09-13 18:34:40 +1000

    add usage examples functionality

--- linguista/vocabulary/permissions.py
+++ linguista/vocabulary/permissions.py
@@ -1,13 +1,23 @@
 from rest_framework.permissions import BasePermission
 
-from core.constants import MAX_DEFINITIONS_AMOUNT
+from core.constants import MAX_DEFINITIONS_AMOUNT, MAX_USAGE_EXAMPLES_AMOUNT
 
 
 class CanAddDefinitionPermission(BasePermission):
-    message = 'You can add no more than 10 definitions to a word'
+    message = f'You can add no more than {MAX_DEFINITIONS_AMOUNT} definitions to a word'
 
     def has_permission(self, request, view):
         word = view.get_object()
         if request.method == 'POST':
             return word.definitions.count() < MAX_DEFINITIONS_AMOUNT
+        return True
+
+
+class CanAddUsageExamplePermission(BasePermission):
+    message = f'You can add no more than {MAX_USAGE_EXAMPLES_AMOUNT} usage examples to a word'
+
+    def has_permission(self, request, view):
+        word = view.get_object()
+        if request.method == 'POST':
+            return word.examples.count() < MAX_USAGE_EXAMPLES_AMOUNT
         return True
