d773229dcf83129892983590a52684b3299a2e2d
Marina <vorona.2003.bu@yandex.ru>
2023-09-22 14:37:44 +0300

    remove linguista dir

--- linguista/exercises/admin.py
+++
@@ -1,8 +0,0 @@
-''' Exercises admin config '''
-
-from django.contrib import admin
-
-from .models import Exercise, FavoriteExercise
-
-admin.site.register(Exercise)
-admin.site.register(FavoriteExercise)
--- linguista/languages/admin.py
+++
@@ -1,8 +0,0 @@
-''' Languages admin config '''
-
-from django.contrib import admin
-
-from .models import Language, UserLanguage
-
-admin.site.register(Language)
-admin.site.register(UserLanguage)
