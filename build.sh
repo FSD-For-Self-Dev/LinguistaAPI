46a1ea87f7b4dd771d208a500194ee8bf9303abd
Marina <vorona.2003.bu@yandex.ru>
2023-09-22 14:37:59 +0300

    update render build

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 poetry install
 
-python linguista/manage.py collectstatic --no-input
-python linguista/manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
