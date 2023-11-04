ad7be0d8922b7d16b3c84549270fa475793503fa
Marina <vorona.2003.bu@yandex.ru>
2023-11-04 22:59:15 +0300

    add slugify method in utils for collections

--- vocabulary/utils.py
+++ vocabulary/utils.py
@@ -1,6 +1,13 @@
 """Утилиты приложения vocabulary."""
 
+from django.template.defaultfilters import slugify
+
 def slugify_text_author_fields(self):
     """Генерация слагов для слов."""
-    without_spaces = self.text.replace(' ', '-')
-    return f'{without_spaces}-{self.author.id}'
+    slugified_text = slugify(self.text)
+    return f'{slugified_text}-{self.author.id}'
+
+def slugify_title_author_fields(self):
+    """Генерация слагов для коллекций."""
+    slugified_title = slugify(self.title)
+    return f'{slugified_title}-{self.author.id}'
