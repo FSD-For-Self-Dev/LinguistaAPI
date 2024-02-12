69d5d6ba36bd99c2892adef04b55e177284ebf99
marin-admin <admin@example.com>
2024-02-12 11:36:30 +0300

    fix users password hashing in admin panel; update CustomUserAdmin using django UserAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,13 +1,14 @@
-''' Users admin config '''
+"""Users admin config."""
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    '''Админ-панель модели пользователя'''
+class CustomUserAdmin(UserAdmin):
+    """Админ-панель модели пользователя"""
 
     list_display = (
         'username',
@@ -27,3 +28,45 @@ class UserAdmin(admin.ModelAdmin):
     )
     ordering = ('-date_joined',)
     prepopulated_fields = {'slug': ('username',)}
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (
+            'Personal info',
+            {
+                'fields': (
+                    'email',
+                    'slug',
+                    'gender',
+                )
+            },
+        ),
+        (
+            'Permissions',
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                )
+            },
+        ),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'username',
+                    'email',
+                    'password1',
+                    'password2',
+                    'slug',
+                ),
+            },
+        ),
+    )
