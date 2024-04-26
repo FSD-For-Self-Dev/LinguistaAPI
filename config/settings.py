0c23cc2b323e2e2804f126479e87710b0ac55743
marina <vorona.2003.bu@yandex.ru>
2024-04-26 15:18:18 +0300

    update workflow

--- config/settings.py
+++ config/settings.py
@@ -88,9 +88,9 @@ DATABASES = {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('DB_USER', default=''),
-        'PASSWORD': os.getenv('DB_PASSWORD', default=''),
+        'PASSWORD': os.getenv('DB_PASSWORD', default='postgres'),
         'HOST': os.getenv('DB_HOST', default=''),
-        'PORT': os.getenv('DB_PORT', default=''),
+        'PORT': os.getenv('DB_PORT', default='5432'),
     }
 }
 
