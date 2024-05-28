1ed9a316d639891e26461b8712477afd87ce49b5
marina <vorona.2003.bu@yandex.ru>
2024-05-28 09:12:03 +0300

    upd: vocabulary admin update

--- vocabulary/admin.py
+++ vocabulary/admin.py
@@ -2,7 +2,7 @@
 
 from django.contrib import admin
 
-from modeltranslation.admin import TranslationAdmin
+from modeltranslation.admin import TabbedTranslationAdmin
 
 from .models import (
     Antonym,
@@ -26,6 +26,9 @@ from .models import (
     WordTranslation,
     WordTranslations,
     WordUsageExamples,
+    QuoteAssociation,
+    WordImageAssociations,
+    WordQuoteAssociations,
 )
 
 
@@ -33,7 +36,7 @@ class WordTranslationInline(admin.TabularInline):
     model = WordTranslations
 
 
-class WordWordsFormGroupsInline(admin.TabularInline):
+class WordFormGroupsInline(admin.TabularInline):
     model = WordsFormGroups
 
 
@@ -65,15 +68,28 @@ class WordDefinitionsInline(admin.TabularInline):
     model = WordDefinitions
 
 
+class NoteInline(admin.TabularInline):
+    prepopulated_fields = {'slug': ('text',)}
+    model = Note
+
+
+class WordImageAssociationsInline(admin.TabularInline):
+    model = WordImageAssociations
+
+
+class WordQuoteAssociationsInline(admin.TabularInline):
+    model = WordQuoteAssociations
+
+
 @admin.register(Word)
 class WordAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('text', 'author')}
-    list_display = ('pk', 'text', 'author')
-    list_display_links = ('text',)
+    list_display = ('slug', 'text', 'author')
+    list_display_links = ('slug',)
     search_fields = ('text', 'author__username')
     list_filter = ('author',)
     inlines = (
-        WordWordsFormGroupsInline,
+        WordFormGroupsInline,
         WordTranslationInline,
         WordUsageExamplesInline,
         WordDefinitionsInline,
@@ -81,14 +97,17 @@ class WordAdmin(admin.ModelAdmin):
         AntonymInline,
         FormInline,
         SimilarInline,
+        NoteInline,
+        WordImageAssociationsInline,
+        WordQuoteAssociationsInline,
     )
 
 
 @admin.register(Type)
-class TypeAdmin(TranslationAdmin):
+class TypeAdmin(TabbedTranslationAdmin):
     prepopulated_fields = {'slug': ('name',)}
-    list_display = ('name', 'words_count')
-    list_display_links = ('name',)
+    list_display = ('slug', 'name', 'words_count')
+    list_display_links = ('slug',)
 
 
 class CollectionWordInLine(admin.TabularInline):
@@ -98,8 +117,8 @@ class CollectionWordInLine(admin.TabularInline):
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title', 'author')}
-    list_display = ('title', 'author', 'words_count')
-    list_display_links = ('title',)
+    list_display = ('slug', 'title', 'author', 'words_count')
+    list_display_links = ('slug',)
     inlines = (CollectionWordInLine,)
 
 
@@ -112,50 +131,106 @@ class TagAdmin(admin.ModelAdmin):
 @admin.register(FormsGroup)
 class FormsGroupAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', 'author')}
-    list_display = ('id', 'name', 'author', 'words_count')
-    list_display_links = ('name',)
+    list_display = ('slug', 'name', 'author', 'words_count')
+    list_display_links = ('slug',)
 
 
 @admin.register(WordTranslation)
 class WordTranslationAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('text', 'author')}
-    list_display = ('id', 'text', 'language', 'words_count')
-    list_display_links = ('text',)
+    list_display = ('slug', 'text', 'language', 'words_count')
+    list_display_links = ('slug',)
 
 
 @admin.register(UsageExample)
 class UsageExampleAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('text', 'author')}
-    list_display = ('id', 'text', 'translation', 'words_count')
-    list_display_links = ('text',)
+    list_display = ('slug', 'text', 'translation', 'words_count')
+    list_display_links = ('slug',)
 
 
 @admin.register(Definition)
 class DefinitionAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('text', 'author')}
-    list_display = ('id', 'text', 'translation', 'words_count')
-    list_display_links = ('text',)
-
-
-@admin.register(Synonym)
-class SynonymAdmin(admin.ModelAdmin):
-    list_display = ('id', 'from_word', 'to_word', 'note')
-    list_display_links = ('from_word',)
+    list_display = ('slug', 'text', 'translation', 'words_count')
+    list_display_links = ('slug',)
 
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'text', 'word', 'created')
-    list_display_links = ('text', 'word')
+    list_display = ('slug', 'text', 'word', 'created')
+    list_display_links = ('slug',)
 
 
-admin.site.register(WordTranslations)
-admin.site.register(WordUsageExamples)
-admin.site.register(WordDefinitions)
-admin.site.register(WordsInCollections)
-admin.site.register(Antonym)
-admin.site.register(Form)
-admin.site.register(Similar)
-admin.site.register(ImageAssociation)
-admin.site.register(FavoriteWord)
-admin.site.register(FavoriteCollection)
+@admin.register(ImageAssociation)
+class ImageAssociationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(QuoteAssociation)
+class QuoteAssociationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Synonym)
+class SynonymAdmin(admin.ModelAdmin):
+    list_display = ('from_word', 'to_word', 'note')
+    list_display_links = ('from_word',)
+
+
+@admin.register(Antonym)
+class AntonymAdmin(admin.ModelAdmin):
+    list_display = ('from_word', 'to_word', 'note')
+    list_display_links = ('from_word',)
+
+
+@admin.register(Form)
+class FormAdmin(admin.ModelAdmin):
+    list_display = ('from_word', 'to_word')
+    list_display_links = ('from_word',)
+
+
+@admin.register(Similar)
+class SimilarAdmin(admin.ModelAdmin):
+    list_display = ('from_word', 'to_word')
+    list_display_links = ('from_word',)
+
+
+@admin.register(FavoriteWord)
+class FavoriteWordAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FavoriteCollection)
+class FavoriteCollectionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(WordTranslations)
+class WordTranslationsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(WordUsageExamples)
+class WordUsageExamplesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(WordDefinitions)
+class WordDefinitionsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(WordsInCollections)
+class WordsInCollectionsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(WordImageAssociations)
+class WordImageAssociationsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(WordQuoteAssociations)
+class WordQuoteAssociationsAdmin(admin.ModelAdmin):
+    pass
