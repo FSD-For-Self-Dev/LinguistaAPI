838fc57a63f4b4c8a5db14018c4e42edd2aec63f
Gregory Shevchuk <2097255@mail.ru>
2024-06-13 05:32:29 +0300

    keyboard fix

--- linguista_bot/keyboards/keyboards.py
+++ linguista_bot/keyboards/keyboards.py
@@ -1,6 +1,7 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
+# стартовая клавиатура
 initial_kb = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text='Я хочу зарегистрироваться')],
@@ -11,6 +12,7 @@ initial_kb = ReplyKeyboardMarkup(
     input_field_placeholder='Авторизуйтесь или создайте учётную запись',
 )
 
+# главная клавиатура
 main_kb = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text='Профиль')],
@@ -23,6 +25,7 @@ main_kb = ReplyKeyboardMarkup(
     input_field_placeholder='Выберите пункт меню',
 )
 
+# клавиатура профиля
 profile_kb = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text='Добавить родной язык')],
@@ -34,6 +37,49 @@ profile_kb = ReplyKeyboardMarkup(
     input_field_placeholder='Выберите пункт меню',
 )
 
+# главная клавитура словаря
+vocabulary_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Просмотр словаря')],
+        [KeyboardButton(text='Поиск по словарю, сортировка, фильтры')],
+        [KeyboardButton(text='Просмотр профиля слова')],
+        [KeyboardButton(text='Добавление одного слова')],
+        [KeyboardButton(text='Добавление нескольких слов')],
+        [KeyboardButton(text='Добавление слов в избранное, удаление из избранного')],
+        [KeyboardButton(text='Просмотр избранных слов')],
+        [KeyboardButton(text='Добавление дополнений к слову')],
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True,
+    input_field_placeholder='Выберите пункт меню',
+)
+
+# главная клавитура коллекий
+collection_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Список коллекций')],
+        [KeyboardButton(text='Просмотр коллекции')],
+        [KeyboardButton(text='Поиск, сортировка, фильтры')],
+        [KeyboardButton(text='Создание коллекции')],
+        [KeyboardButton(text='Добавление слов в коллекцию')],
+        [KeyboardButton(text='Добавление коллекции в избранное, удаление из избранного')],
+        [KeyboardButton(text='Просмотр избранных коллекций')],
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True,
+    input_field_placeholder='Выберите пункт меню',
+)
+
+# главная клавитура упражнений
+excercise_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Выбор упражнения (библиотека упражнений)')],
+        [KeyboardButton(text='Прохождение упражнения Переводчик')],
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True,
+    input_field_placeholder='Выберите пункт меню',
+)
 
 def auth_kb(text: str | list):
     builder = ReplyKeyboardBuilder()
@@ -42,12 +88,12 @@ def auth_kb(text: str | list):
     [builder.button(txt) for txt in text]
     return builder.as_markup(resize_keyboard=True, one_time_keyboard=True)
 
-
+# клавитатура отмены?
 cancel_kb = ReplyKeyboardMarkup(
     keyboard=[[KeyboardButton(text='Отмена')]],
     resize_keyboard=True,
     one_time_keyboard=True,
 )
 
-
+# удаление клавиатуры
 rmk = ReplyKeyboardRemove()
