8543864c926bdbc7834ca210260d1d270b58a4f4
marina <vorona.2003.bu@yandex.ru>
2024-07-31 18:20:57 +0300

    config: databases config component small fix

--- config/components/databases.py
+++ config/components/databases.py
@@ -1,7 +1,7 @@
 import os
 import dj_database_url
 
-DATABASE_URL = os.getenv('DATABASE_URL', '')
+DATABASE_URL = os.getenv('DATABASE_URL', default='')
 
 DATABASES = {
     'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
