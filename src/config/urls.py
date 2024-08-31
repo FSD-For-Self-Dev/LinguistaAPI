2a2771dec711b7d7c396ea1fa14c50e0e41548f2
marina <vorona.2003.bu@yandex.ru>
2024-08-31 12:04:26 +0300

    fix/main urls update

--- src/config/urls.py
+++ src/config/urls.py
@@ -6,26 +6,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularSwaggerView,
-    SpectacularRedocView,
-)
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path(
-        'api/v1/schema/swagger-ui/',
-        SpectacularSwaggerView.as_view(url_name='schema'),
-        name='swagger-ui',
-    ),
-    path(
-        'api/v1/schema/redoc/',
-        SpectacularRedocView.as_view(url_name='schema'),
-        name='redoc',
-    ),
 ]
 
 urlpatterns += i18n_patterns(
