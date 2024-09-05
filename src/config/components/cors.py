ee44b1cf5d2e4de0e4c43e4c5f382091c0121224
marina <vorona.2003.bu@yandex.ru>
2024-09-05 20:00:44 +0300

    fix/cors update

--- src/config/components/cors.py
+++ src/config/components/cors.py
@@ -4,6 +4,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost',
     'https://fsd-for-self-dev.github.io',
     'http://localhost:3030',
+    'https://linguista.online',
     # Дополнительные разрешенные источники, если есть
 ]
 
