f6b2982abae419d2b18b377e5c13c10587d03661
marina <vorona.2003.bu@yandex.ru>
2024-11-01 14:04:01 +0300

    fix/update image compress func

--- src/utils/images.py
+++ src/utils/images.py
@@ -1,24 +1,31 @@
 """Utils to do something with images."""
 
-from typing import Any, Type
+from typing import Type
 
 from io import BytesIO
 from PIL import Image
 from django.core.files import File
+from django.core.files.base import ContentFile
 
 
-def compress(image: Any) -> Type[File]:
+def compress(image: File) -> Type[File]:
     """Returns compressed image as django-friendly File object."""
     img = Image.open(image)
 
+    img.resize((200, 300), Image.Resampling.LANCZOS)
+
     # create a BytesIO object
-    thumb_io = BytesIO()
+    img_bytes = BytesIO()
+
     # save image to BytesIO object
     if img.mode != 'RGB':
         img = img.convert('RGB')
-    img = img.save(thumb_io, 'JPEG', quality=70, optimize=True)
+
+    img.save(fp=img_bytes, format='JPEG', quality=25, optimize=True)
+    image_content_file = ContentFile(content=img_bytes.getvalue())
+    name = image.name.split('.')[0] + '.jpg'
 
     # create a django-friendly Files object
-    thumbnail = File(thumb_io, name=image.name)
+    thumbnail = File(image_content_file, name=name)
 
     return thumbnail
