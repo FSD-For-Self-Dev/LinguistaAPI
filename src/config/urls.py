b08bf1677d2ebe02b46dc2dc457da0a67b487e05
marina <vorona.2003.bu@yandex.ru>
2024-12-16 13:31:24 +0300

    fix/add "admin/" path to i18n_patterns

--- src/config/urls.py
+++ src/config/urls.py
@@ -7,12 +7,12 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('unsplash/', include('library.unsplash_api.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
 ]
 
 urlpatterns += i18n_patterns(
+    path('admin/', admin.site.urls),
     path('api/', include('api.v1.urls')),
     # Save the prefix for the default language
     prefix_default_language=True,
